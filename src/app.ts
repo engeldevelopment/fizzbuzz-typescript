3bb0b2990ed54b1d18f0a007311363937059bb9b
engel <engeljavierpinto@gmail.com>
2021-03-27 14:40:41 -0400

    refactor: Solo deje publico el metodo valueOfNumber.

--- src/app.ts
+++ src/app.ts
@@ -13,15 +13,15 @@ export default class FizzBuzzCalculator {
         return message
     }
 
-    isMultipleOfThree(number: number): boolean {
+    private isMultipleOfThree(number: number): boolean {
         return (number % 3) == 0
     }
 
-    isMultipleOfFive(number: number): boolean {
+    private isMultipleOfFive(number: number): boolean {
         return (number % 5) == 0
     }
 
-    isMultipleOfBoth(number: number) {
+    private isMultipleOfBoth(number: number) {
         return this.isMultipleOfThree(number) && this.isMultipleOfFive(number)
     }
 }
